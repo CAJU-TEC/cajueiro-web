e2b2ac085fd7cf057e1c00bfbabea631149e538f
Luizmagao <luizmagao@gmail.com>
2023-03-02 19:55:25 -0300

    :art: Modificando finalizando para  finalizando

--- src/support/tickets/status.js
+++ src/support/tickets/status.js
@@ -5,5 +5,5 @@ export default {
   'development': {'color': 'deep-orange-14', 'hex': '#C70039', 'title': 'DESENVOLVIMENTO'},
   'test': {'color': 'red-14', 'hex': '#900C3F', 'title': 'TESTE'},
   'pending': {'color': 'brown-10', 'hex': '#581845', 'title': 'PENDENTE'},
-  'done': {'color': 'blue-grey-2', 'hex': '#A6A9A7', 'title': 'FINALIZANDO'},
+  'done': {'color': 'blue-grey-2', 'hex': '#A6A9A7', 'title': 'FINALIZADO'},
 };
