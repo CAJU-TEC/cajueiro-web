cdc54a415d03af48d37326a1cbca128604721be6
Luizmagao <luizmagao@gmail.com>
2023-03-28 15:54:24 -0300

    :art: atualizando graficos

--- src/components/charts/apexBar.vue
+++ src/components/charts/apexBar.vue
@@ -1,33 +1,98 @@
 <template>
-  <div>
-    <apexchart height="300" type="bar" :options="reactiveValue"></apexchart>
-    {{ reactiveValue }}
-  </div>
+  <apexchart
+    class="col"
+    height="300"
+    type="bar"
+    :options="options"
+    :series="options.series"
+  ></apexchart>
 </template>
 
 <script>
-import { computed, defineComponent, reactive, ref, toRef, watch } from 'vue';
+import { defineComponent, onMounted, reactive, ref, watch } from 'vue';
+import _ from 'lodash';
+import ticketsService from 'src/services/tickets';
+
 export default defineComponent({
   name: 'ApexBar',
-  props: {
-    optionsProps: {
-      type: Object,
-      default: null,
-    },
-  },
-  setup(props) {
-    const reactiveValue = ref(props.optionsProps);
+  setup() {
+    const { list } = ticketsService();
+    const tickets = ref([]);
+    const options = ref({
+      series: [
+        {
+          data: [],
+        },
+      ],
+      chart: {
+        type: 'bar',
+        height: 350,
+      },
+      plotOptions: {
+        bar: {
+          borderRadius: 4,
+          horizontal: true,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      xaxis: {
+        categories: [],
+      },
+    });
 
-    watch(
-      () => props.optionsProps,
-      (newValue, oldValue) => {
-        console.log(newValue, oldValue);
-        reactiveValue.value = newValue;
+    const getTickets = async () => {
+      try {
+        const data = list();
+        tickets.value = await data;
+        options.value = {
+          xaxis: {
+            categories: [...recoverCollaborators().map((v) => v.user)],
+          },
+          series: [
+            {
+              data: recoverCollaborators().map((v) => v.count),
+            },
+          ],
+        };
+      } catch (error) {
+        console.log(error);
       }
-    );
+    };
+
+    onMounted(async () => {
+      await getTickets();
+    });
+
+    const recoverCollaborators = () => {
+      const listCollaboratorGroupBy = ref(
+        _.chain(tickets.value)
+          .groupBy('collaborator_id')
+          .map((value) => recoverUser(value))
+          .filter((value) => value.user !== undefined)
+          .value()
+      );
+
+      return listCollaboratorGroupBy.value;
+    };
+
+    const recoverUser = (collaborator) => {
+      const payload = reactive({
+        user: null,
+        count: 0,
+      });
+
+      _.forEach(collaborator, function (value) {
+        payload.user = value?.collaborator?.first_name;
+        payload.count = collaborator?.length;
+      });
+
+      return payload;
+    };
 
     return {
-      reactiveValue,
+      options,
     };
   },
 });
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,94 +6,21 @@
       </q-breadcrumbs>
       <h2 class="pt-10 text-h4">Galerinha em atividades!</h2>
       <div class="row q-col-gutter-sm">
-        <apex-bar height="300" :optionsProps="options"></apex-bar>
+        <apex-bar height="300"></apex-bar>
       </div>
     </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent, ref, onMounted, reactive } from 'vue';
+import { defineComponent } from 'vue';
 import ApexBar from 'src/components/charts/apexBar.vue';
-import ticketsService from 'src/services/tickets';
-import _ from 'lodash';
 
 export default defineComponent({
   name: 'IndexPage',
   components: { ApexBar },
   setup() {
-    const tickets = ref([]);
-    const { list } = ticketsService();
-    const options = reactive({
-      series: [
-        {
-          data: [1, 2],
-        },
-      ],
-      chart: {
-        type: 'bar',
-        height: 350,
-      },
-      plotOptions: {
-        bar: {
-          borderRadius: 4,
-          horizontal: true,
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      xaxis: {
-        categories: [1, 2],
-      },
-    });
-
-    const getTickets = async () => {
-      try {
-        const data = await list();
-        tickets.value = data;
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    onMounted(async () => {
-      await getTickets();
-      // const result = ref(recoverCollaborators());
-
-      // options.xaxis.categories = await result.value.map((v) => v.user);
-      // options.count = await result.value.map((v) => v.count);
-    });
-
-    const recoverCollaborators = () => {
-      const listCollaboratorGroupBy = ref(
-        _.chain(tickets.value)
-          .groupBy('collaborator_id')
-          .map((value) => recoverUser(value))
-          .filter((value) => value.user !== undefined)
-          .value()
-      );
-
-      return listCollaboratorGroupBy.value;
-    };
-
-    const recoverUser = (collaborator) => {
-      const payload = reactive({
-        user: null,
-        count: 0,
-      });
-
-      _.forEach(collaborator, function (value) {
-        payload.user = value?.collaborator?.first_name;
-        payload.count = collaborator?.length;
-      });
-
-      return payload;
-    };
-
-    return {
-      options,
-    };
+    return {};
   },
 });
 </script>
