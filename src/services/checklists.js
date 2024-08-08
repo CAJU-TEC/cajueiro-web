fda4fc2d561d32b5eed35d4945b3c9ea75482e65
Luizmagao <luizmagao@gmail.com>
2024-08-08 17:14:05 -0300

    :art: Check Lists

--- src/services/checklists.js
+++ src/services/checklists.js
@@ -1,6 +1,7 @@
 import useApi from 'src/composables/UseApi';
 
 export default function checkListsService() {
+
   const endpoint = 'api/checkLists';
   const { list, getById, post, update, remove } = useApi(endpoint);
 
