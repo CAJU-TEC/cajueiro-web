1bb4d3fbe411ecd2f1a3183cbcea7bb59a7302bf
Luizmagao <luizmagao@gmail.com>
2024-08-08 17:45:09 -0300

    :art: Alter file to checklist

--- src/pages/tickets/CheckList.vue
+++ src/pages/tickets/CheckList.vue
@@ -78,7 +78,7 @@
 
 <script>
 import { defineComponent, ref, onMounted } from 'vue';
-import checkListsService from 'src/services/checkLists';
+import checkListsService from 'src/services/checkList';
 import { useQuasar } from 'quasar';
 import { useRouter } from 'vue-router';
 
