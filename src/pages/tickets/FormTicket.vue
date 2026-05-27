4e00545a335b03f44422211894fdfc4f76fef292
caju011 <caju011@caju011.local>
2026-05-27 08:35:12 -0300

    fix(tickets): corrige listagem de clientes ao criar ticket

--- src/pages/tickets/FormTicket.vue
+++ src/pages/tickets/FormTicket.vue
@@ -314,8 +314,9 @@ export default defineComponent({
     const router = useRouter();
     const route = useRoute();
     const dialogAddClient = ref(false);
-    const optionsClient = ref();
-    const stringOptionsClient = ref(optionsClient);
+    const optionsClient = ref([]);
+    const stringOptionsClient = ref([]);
+
     const optionsImpacts = ref();
     const optionsStatus = ref([
       { value: 'backlog', label: 'Aguardando' },
@@ -349,6 +350,7 @@ export default defineComponent({
         getClient(route.params.id);
       }
       getClientSelect();
+
       getImpactSelect();
     });
 
@@ -396,6 +398,7 @@ export default defineComponent({
             label: `${m.full_name} (${m.email?.description})`,
           };
         });
+        stringOptionsClient.value = optionsClient.value;
       } catch (error) {
         $q.notify({
           message: 'Ops! Ocorreu algum erro.',
